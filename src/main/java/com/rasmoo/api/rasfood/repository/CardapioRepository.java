fc8b7093c00f85b70d97e176ec85bd1b0a823309
victorrti <victorrodriguesti@gmail.com>
2024-11-15 18:09:46 -0300

    criando update com native query e utilizando transaction e modify

--- src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
+++ src/main/java/com/rasmoo/api/rasfood/repository/CardapioRepository.java
@@ -3,8 +3,11 @@ package com.rasmoo.api.rasfood.repository;
 import com.rasmoo.api.rasfood.dto.CardapioDto;
 import com.rasmoo.api.rasfood.entity.Cardapio;
 import com.rasmoo.api.rasfood.repository.projection.CardapioProjection;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,4 +24,8 @@ public interface CardapioRepository extends JpaRepository<Cardapio,Integer> {
             " join categorias cat on c.id = cat.id " +
             "where c.id = ?id",nativeQuery = true)
     List<CardapioProjection> findAllByCategoria(final Integer id);
+    @Modifying
+    @Transactional
+    @Query("UPDATE cardapio c set c = CASE c.dispobilidade WHEN treu THEN false ELSE true END WHERE c.id = :id ")
+    Integer updateDisponibilidade(@Param("id") Integer id);
 }
